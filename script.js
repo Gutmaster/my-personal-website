fb5a1cc94f287c14ef818bb3fb4c0f6becaf4794
+ <+>
2023-12-15 09:25:00 -0800

    deletable chars and color change on empty delete

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
+const body = document.querySelector("body")
+
 const h2 = document.createElement("h2");
+
 h2.textContent = "This content added by JavaScript";
 
-document.querySelector("body").appendChild(h2);
+body.appendChild(h2);
 
 document.addEventListener("keydown", function (event) {
-    console.log(event);
-    const newChar = document.createElement("p");
-    newChar.style.fontSize = "25px";
-    newChar.textContent = event.key;
-    document.querySelector("body").appendChild(newChar);
+    if (event.key === "Delete"){
+        children = this.getElementsByClassName("chars")
+        console.log(children.length)
+        if(children.length <= 0){
+            body.style.background = "red"
+        }else{
+            for(i=children.length-1; i>=children.length; i--)
+                console.log(children.length, " removing" + children[i])
+                body.removeChild(children[i])
+        }
+    }
+    else
+    {
+        const newChar = document.createElement("p");
+        newChar.classList.add("chars")
+        newChar.textContent = event.key;
+        body.appendChild(newChar);
+    }
 });
