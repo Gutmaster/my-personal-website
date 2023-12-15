bc5bdf56e19b53bbb1ec7b10b6378d4461a0a661
+ <+>
2023-12-15 09:04:58 -0800

    key display column

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 
 document.querySelector("body").appendChild(h2);
+
+document.addEventListener("keydown", function (event) {
+    console.log(event);
+    const newChar = document.createElement("p");
+    newChar.style.fontSize = "25px";
+    newChar.textContent = event.key;
+    document.querySelector("body").appendChild(newChar);
+});
